faf61e96e280e81eed908f1f84f911f23a5591c5
Piyal Basu <pbasu235@gmail.com>
2021-08-06 16:44:23 -0400

    add configurable `tracingOrigins` to error reporting (#6)

--- src/helpers/errorReporting.ts
+++ src/helpers/errorReporting.ts
@@ -3,19 +3,37 @@ import { Integrations } from "@sentry/tracing";
 
 /**
  *
- * @param {string} projectName The name of the project used in the release tag
- * @param {string?} releaseVersion An optional release version. Omit this param
+ * @param {string} obj.projectName The name of the project used in the release tag
+ * @param {string?} obj.releaseVersion An optional release version. Omit this param
  * if your app supports dynamic version tracking
+ * @param {Array<string>} obj.tracingOrigins Array of domains to pass tracing origins to
  * @returns {void}
  */
-const reportErrors = (projectName: string, releaseVersion?: string) => {
+
+interface ReportErrors {
+  projectName: string;
+  releaseVersion?: string;
+  tracingOrigins?: Array<string>;
+}
+
+const reportErrors = ({
+  projectName,
+  releaseVersion,
+  tracingOrigins,
+}: ReportErrors) => {
   if (process.env.REACT_APP_SENTRY_KEY) {
     Sentry.init({
       dsn: process.env.REACT_APP_SENTRY_KEY,
       release: `${projectName}@${
         releaseVersion || process.env.npm_package_version
       }`,
-      integrations: [new Integrations.BrowserTracing()],
+      integrations: [
+        tracingOrigins
+          ? new Integrations.BrowserTracing({
+              tracingOrigins,
+            })
+          : new Integrations.BrowserTracing(),
+      ],
       tracesSampleRate: 1.0,
     });
   }
